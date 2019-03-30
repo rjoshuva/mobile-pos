bbd8b935fce071a7a83bbfd7943038fe59c36cdf
Noah Holt <noahryanholt@gmail.com>
2019-03-30 14:58:32 -0500

    Fixed the frontend to use employee id instead of names

--- public/scripts/createEmployee.js
+++ public/scripts/createEmployee.js
@@ -6,109 +6,102 @@ let level = decodeURI(getCookie("level"));
 let employeesExist = false;
 
 if (employeesExist)
-	titleText.textContent = "Create a new employee:";
+    titleText.textContent = "Create a new employee:";
 else
-	titleText.textContent = "Please create your first employee:";
+    titleText.textContent = "Please create your first employee:";
 
 $(document).ready(() => {
 
-	/*
-	GET TEST
-	$.get(backendURL + "/api/employee", (result) => {
-		alert(result);
-	});
-	*/
+    /*
+    GET TEST
+    $.get(backendURL + "/api/employee", (result) => {
+        alert(result);
+    });
+    */
 
-	$("#saveUser").click(() => {
-		let firstName = document.getElementById("firstName").value;
-		let lastName = document.getElementById("lastName").value;
-		let password = document.getElementById("password").value;
-	
-		saveUser(firstName, lastName, password);
-	});
+    $("#saveUser").click(() => {
+        let firstName = document.getElementById("firstName").value;
+        let lastName = document.getElementById("lastName").value;
+        let password = document.getElementById("password").value;
+
+        saveUser(firstName, lastName, password);
+    });
 });
 
-function createButton(text, clickFunc)
-{
-	let newButton = document.createElement("button");
-	newButton.textContent = text;
-	newButton.onclick = clickFunc;
-	return newButton;
+function createButton(text, clickFunc) {
+    let newButton = document.createElement("button");
+    newButton.textContent = text;
+    newButton.onclick = clickFunc;
+    return newButton;
 }
 
-function saveUser(firstName, lastName, password)
-{
-	//{"lastName": "Holt", "firstName": "Steve", "classification": "Jock", "password": "bigyeet101"}'
+function saveUser(firstName, lastName, password) {
+    //{"lastName": "Holt", "firstName": "Steve", "classification": "Jock", "password": "bigyeet101"}'
 
-	/*
-	$.post(backendURL + "/api/employee",
-	{
-		lastName: lastName,
-		firstName: firstName,
-		classification: "General Manager",
-		active: "1",
-		password: password
-	}, function (data, callbackResponse) {
-		console.log("Status: " + callbackResponse);
-		if (callbackResponse
-			&& callbackResponse.status
-			&& (callbackResponse.status >= 200)
-			&& (callbackResponse.status < 300))
-		{
-			setCookie("firstName", firstName);
-			setCookie("classification", saveUserRequest.classification);
-			location.href = "home.html";
-		}
-	});*/
+    /*
+    $.post(backendURL + "/api/employee",
+    {
+        lastName: lastName,
+        firstName: firstName,
+        classification: "General Manager",
+        active: "1",
+        password: password
+    }, function (data, callbackResponse) {
+        console.log("Status: " + callbackResponse);
+        if (callbackResponse
+            && callbackResponse.status
+            && (callbackResponse.status >= 200)
+            && (callbackResponse.status < 300))
+        {
+            setCookie("firstName", firstName);
+            setCookie("classification", saveUserRequest.classification);
+            location.href = "home.html";
+        }
+    });*/
 
-	let newEmployeeData = {
-		lastName: lastName,
-		firstName: firstName,
-		classification: "General Manager",
-		active: "1",
-		password: password
-	}
+    let newEmployeeData = {
+        lastName: lastName,
+        firstName: firstName,
+        classification: "General Manager",
+        active: "1",
+        password: password
+    }
 
-	$.ajax({
-		type: "POST",
-		cache: "FALSE",
-		dataType: "text",
-		//contentType: "application/json",
-		url: backendURL + "/api/employee",
-		data: newEmployeeData,
-		complete: (xhr, statusText) => {
-			//console.log(xhr);
-			console.log(statusText);
-		}
-	});
+    $.ajax({
+        type: "POST",
+        cache: "FALSE",
+        dataType: "text",
+        //contentType: "application/json",
+        url: backendURL + "/api/employee",
+        data: newEmployeeData,
+        complete: (xhr, statusText) => {
+            //console.log(xhr);
+            console.log(statusText);
+        }
+    });
 }
 
-function getCookie(cname)
-{
-	var name = cname + "=";
-	var decodedCookie = decodeURIComponent(document.cookie).replace(/\+/g, ' ');
-	var ca = decodedCookie.split(';');
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie).replace(/\+/g, ' ');
+    var ca = decodedCookie.split(';');
 
-	for(var i = 0; i <ca.length; i++)
-	{
-		var c = ca[i];
-		while (c.charAt(0) == ' ')
-		{
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0)
-		{
-			return c.substring(name.length, c.length);
-		}
-	}
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
 
-	return "";
+    return "";
 }
 
-function setCookie(cname, cvalue, exdays)
-{
-	var d = new Date();
-	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	var expires = "expires="+ d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -13,7 +13,6 @@ $(document).ready(() => {
         let hash = sha256.create();
         hash.update(password);
         let hashedPasswordAttempt = hash.hex();
-
         tryLogin(id, hashedPasswordAttempt);
     });
 
@@ -24,21 +23,21 @@ $(document).ready(() => {
 
 function tryLogin(userID, passwordAttempt) {
     $.get(backendURL + "/api/employee", (data) => {
+        console.log(data);
         let matchingEmployee = null;
-
         $.each(data, (index, employee) => {
-            if (employee.userID === userID && employee.password === passwordAttempt) {
+            if (employee.employeeid === userID && employee.password === passwordAttempt) {
                 matchingEmployee = employee;
             }
         });
 
+        console.log(matchingEmployee);
         if (matchingEmployee !== null) {
             setCookie("firstName", matchingEmployee.firstName);
             setCookie("classification", matchingEmployee.classification);
             location.href = "main.html";
         } else {
-            alert("Incorrect First Name, Last Name, or Password!");
+            alert("Incorrect User Id or Password!");
         }
     });
 }
-
